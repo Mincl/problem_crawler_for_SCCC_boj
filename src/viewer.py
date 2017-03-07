7b4e4a4af7e6d3b5377f5a2818b0f7c0b663cee8
Mincl <eonchan99@gmail.com>
2017-03-07 16:59:04 +0900

    add user's unsolved option to viewer

--- src/viewer.py
+++ src/viewer.py
@@ -1,9 +1,27 @@
+import http.client
 import ast
 import argparse
 from operator import itemgetter
+from bs4 import BeautifulSoup
+
+def get_response_data(host, path):
+    conn = http.client.HTTPSConnection(host)
+    conn.connect()
+    conn.request('GET', path)
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+    return data
+
+def has_class(element, class_name):
+    if 'class' in element.attrs and class_name in element['class']:
+        return True
+    return False
+
 
 HOST = 'www.acmicpc.net'
 GROUP_MEMBER_CNT = 43
+USER_PATH = '/user/%s'
 
 file = open('solved_list', 'r')
 solved_list_str = file.read()
@@ -39,6 +57,7 @@ parser.add_argument('-spc', help="sort by solved per challenger [default]", acti
 parser.add_argument('-cpa', help="sort by challenger per all", action="store_true")
 parser.add_argument('-d', help="sort by descending order [default]", action="store_true")
 parser.add_argument('-a', help="sort by ascending order", action="store_true")
+parser.add_argument('-u', help="grep specific user's unsolved problem list")
 args = parser.parse_args()
 
 column = 'solved_per_challenger'
@@ -52,12 +71,27 @@ if args.a:
     order=False
 
 sorted_list = sorted(rebuild_list, key=itemgetter(column, 'solved_cnt'), reverse=order)
+
+user_solved = []
+if args.u:
+    user_html = get_response_data(HOST, USER_PATH % args.u)
+    soup = BeautifulSoup(user_html, 'html.parser')
+    panel_cnt = 0
+    for div in soup.find_all('div'):
+        if has_class(div, 'panel-body'):
+            panel_cnt = panel_cnt + 1
+            for span in div.find_all('span'):
+                if has_class(span, 'problem_number'):
+                    number=span.a.string
+                    if panel_cnt == 1:
+                        user_solved.append(number)
 for info in sorted_list:
-    print('%2d %2d %6.2f %6.2f %6.2f [%5s - %s]' % (
-        info['solved_cnt'], info['failed_cnt'],
-        info['solved_per_challenger'],
-        info['solved_per_all'],
-        info['challenger_per_all'],
-        info['number'], info['title']
+    if not info['number'] in user_solved:
+        print('%2d %2d %6.2f %6.2f %6.2f [%5s - %s]' % (
+            info['solved_cnt'], info['failed_cnt'],
+            info['solved_per_challenger'],
+            info['solved_per_all'],
+            info['challenger_per_all'],
+            info['number'], info['title']
+            )
         )
-    )
